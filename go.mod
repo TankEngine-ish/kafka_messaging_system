a11f4ba3f0b6340bb8930fd915543cae55affc69
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-04-13 00:07:07 +0300

    finish producer functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,34 @@
 module github.com/TankEngine-ish/Kafka_Messaging_System
 
 go 1.22.1
+
+require (
+	github.com/Shopify/sarama v1.29.1 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.2.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/gofiber/fiber/v2 v2.52.4 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.0.0 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.2 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.52.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
--- producer/producer.go
+++ producer/producer.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+
+	"github.com/Shopify/sarama"
+	"github.com/gofiber/fiber/v2"
 )
 
 type Comment struct {
@@ -15,22 +19,67 @@ func main() {
 	api.Post("/comments", createComment)
 	app.Listen(":3000")
 }
+func ConnectProducer(brokersUrl []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+
+	conn, err := sarama.NewSyncProducer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func PushCommentToQueue(topic string, message []byte) error {
+	brokersUrl := []string{"localhost:29092"}
+	producer, err := ConnectProducer(brokersUrl)
+	if err != nil {
+		return err
+
+	}
+	defer producer.Close()
+	msg := &sarama.ProducerMessage{
+
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+
+		return err
+
+	}
+	fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
+	return nil
+}
 
 func createComment(c *fiber.Ctx) error {
 	cmt := new(Comment)
 	if err := c.BodyParser(cmt); err != nil {
-		log.Panicln(err)
-		c.Status(400).JSON(&fiber.Map)
-		"success":false,
-		"message":err,
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
 		})
 		return err
 	}
 	cmtInBytes, err := json.Marshal(cmt)
 	PushCommentToQueue("comments", cmtInBytes)
 
-	c.JSON(&fiber.Map{
+	err = c.JSON(&fiber.Map{
 		"success": true,
 		"message": "Comment pushed successfully",
-})
+		"comment": cmt,
+	})
+	if err != nil {
+		c.Status(500).JSON(&fiber.Map{
+			"success": false,
+			"message": "Error creating the product",
+		})
+		return err
+	}
+	return err
 }
